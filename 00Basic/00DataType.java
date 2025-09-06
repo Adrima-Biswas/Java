42e66e739fac1cac5fe380c1a04f584f4c592f38
Adrima Biswas <adrimabiswas2003@gmail.com>
2025-09-06 22:18:53 +0530

    Update 00DataType.java

--- 00Basic/00DataType.java
+++ 00Basic/00DataType.java
@@ -1,1 +1,28 @@
+/*
+	1. A data type defines the type of data a variable can store.
+	2. Types of Data Types in Java:
+		a.Primitive Data Type : These are the basic data types 
+								built into the Java language. 
+								They are not objects and represent raw values.
+								Example : 
+								   Data Type     Size      Default value
+									Byte      :  1 byte  :     0          // Byte a; [a = 0]
+									short     :  2 byte  :     0          // short b; [b = 0]
+									int       :  4 byte  :     0          // int c; [c = 0]
+									long      :  8 byte  :     0L
+									float     :  4 byte  :     0.0f
+									double    :  8 byte  :     0.0d
+									char      :  2 byte  :     '\u000' --> Not present in Java
+									boolean   :  1 Bit   :     false
+
+		b.Reference (Non-Primitive) Data Type :  These are more complex data 
+			                                     types and are used to store 
+			                                     references to objects. Unlike 
+			                                     primitive types, reference types 
+			                                     refer to an object or an array in memory.
+
+				Class: A user-defined blueprint or prototype for creating objects.(e.g., String, System)
+				Interface: A contract for what a class can do but without providing the implementation.
+				Array: A collection of similar types of objects.
+*/
 
