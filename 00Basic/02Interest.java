98a486c6ca14f76221c2485b06e044c257e2b989
Adrima Biswas <adrimabiswas2003@gmail.com>
2025-09-07 22:44:18 +0530

    Update 02Interest.java

--- 00Basic/02Interest.java
+++ 00Basic/02Interest.java
@@ -1,1 +1,10 @@
-asd
+class Main{
+ public static void main(String[] args){
+ 	int p , r , t , i;
+ 	p = 1000;
+ 	r = 2;
+ 	t = 2;
+ 	i = (p * r * t)/100;
+ 	System.out.println("the value of i is: "+i);
+ }
+}
