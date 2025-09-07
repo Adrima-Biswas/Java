a31f4d8edae42a08101c0ecb2d38a506e5bc1f2b
Adrima Biswas <adrimabiswas2003@gmail.com>
2025-09-07 22:46:47 +0530

    Update 03Operation.java

--- 00Basic/03Operation.java
+++ 00Basic/03Operation.java
@@ -1,1 +1,10 @@
-a
+class Main{
+ public static void main(String[] args){
+ 	int p , r , t , i;
+ 	p = 1000;
+ 	r = 2;
+ 	t = 2;
+ 	i = (p * r * t)/100;
+ 	System.out.println("the value of i is: "+i);
+ }
+}
