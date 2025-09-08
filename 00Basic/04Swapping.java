520944c1dadc1db51f36be1d00fec967b8e8f5af
Adrima Biswas <adrimabiswas2003@gmail.com>
2025-09-08 23:27:11 +0530

    Update 04Swapping.java

--- 00Basic/04Swapping.java
+++ 00Basic/04Swapping.java
@@ -1,1 +1,35 @@
-a
+/*
+	a = 15;  After Swapping  a = 14;
+	b = 14;				     b = 15;
+*/
+class Main{
+	public static void main(String[] args){
+	 	int a , b;
+	    a = 1;
+	    b = 2;
+
+	    // By using third variable
+	    // int temp = a;
+	    // a = b;
+	    // b = temp;
+
+	    //by using + -
+        a = a + b; // 1 + 2
+        b = a - b; // 1 + 2 - 2 = 1
+        a = a - b; // 1 + 2 - 1 = 2
+
+        // by using * /
+        a = a * b; 
+        b = a / b;
+        a = a / b;
+
+        // by using ^ (xor)
+        a = a ^ b;
+        b = a ^ b; 
+        a = a ^ b;
+	
+	    System.out.println("value of a : "+a);
+	    System.out.println("value of b : "+b);   
+	}
+
+}
