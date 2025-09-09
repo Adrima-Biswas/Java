4d592db074ac99b8df9be9843cc103a633fda7f1
Adrima Biswas <adrimabiswas2003@gmail.com>
2025-09-09 22:22:46 +0530

    Update 00prog.java

--- 01Operator/00prog.java
+++ 01Operator/00prog.java
@@ -1,1 +1,19 @@
-as
+class Main
+{
+	public static void main(String[] args) {
+	   { // Relational Operator : < , <= , >= , > , == , !=
+	       boolean b1 = 4 < 1;
+	       boolean b2 = (4 > 4);
+	       boolean b = (4 != 4);
+	       System.out.print(b);
+	   }
+	   
+	   { // Logical Operator : && , || , !
+	       boolean b1 = (4 > 7) && (6 != 0);
+	       boolean b2 = (4 > 7) || (6 != 6);
+	       boolean b = !(4 > 7);
+	       System.out.print(b);
+	       
+	   }
+	}
+}  
