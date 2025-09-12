0824559ea7b912542ed55c99b050e05609c6cf31
Adrima Biswas <adrimabiswas2003@gmail.com>
2025-09-12 23:25:13 +0530

    Update 01Calling.java

--- 02Method/01Calling.java
+++ 02Method/01Calling.java
@@ -1,14 +1,15 @@
-class Main {
-    public static void fun3(){
+class Main 
+{
+    public static void fun3() {
         System.out.println("you are in fun 3");
     }    
     
-    public static void fun2(){
+    public static void fun2() {
         System.out.println("you are in fun 2");
         fun3();
     }  
     
-    public static void fun1(){
+    public static void fun1() {
         System.out.println("you are in fun 1");
         fun2();
     }
@@ -16,4 +17,5 @@ class Main {
     public static void main(String[] args) {
         fun1();
     }
+    
 }
