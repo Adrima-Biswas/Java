08248da47bb335e8bff9c15317c4ed6c7e70caf1
Adrima Biswas <adrimabiswas2003@gmail.com>
2025-09-12 23:29:24 +0530

    Update temp.java

--- 01Operator/temp.java
+++ 01Operator/temp.java
@@ -1,1 +1,18 @@
-.....
+class Test {
+      public static void main(String[] args) {
+       // a++ : Returns the current value of a, then increments a by 1.
+      // ++a : Increments a by 1 first, then returns the updated value.
+          int a=3;
+          a=a++; // a = 3 then incre a 3 to 4
+          System.out.println("Post-Increment a: " +a);
+
+          a=++a; // 3 => 4 then a = 4
+          System.out.println("Pre-Increment a: "+a );
+
+          a=a--;  // a = 4 then decre a 4 to 3
+          System.out.println("Post-Decrement a: " +a);
+
+          a=--a; // a = 3
+          System.out.println("Pre-Decrement a: "+a );
+      }
+}
