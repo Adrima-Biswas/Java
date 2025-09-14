5552d93421d993d10d87c8a611a692c4d9b93cb2
Adrima Biswas <adrimabiswas2003@gmail.com>
2025-09-14 22:30:09 +0530

    Update temp.java

--- 03Conditional statements/if_else statements/temp.java
+++ 03Conditional statements/if_else statements/temp.java
@@ -1,1 +1,14 @@
-....
+//WAP to print odd and even number
+import java.util.Scanner;
+class oddeven {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter any number : ");
+		int n = sc.nextInt();
+        
+        if(n%2==0)
+        	System.out.println(n + " is the even number");
+        else
+        	System.out.println(n + " is the odd number");
+    }
+}
