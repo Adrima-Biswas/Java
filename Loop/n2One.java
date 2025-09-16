2ec48a39bfa67e47e9af2d76bc4862c2339a5a90
Adrima Biswas <adrimabiswas2003@gmail.com>
2025-09-16 19:50:54 +0530

    Update n2One.java

--- Loop/n2One.java
+++ Loop/n2One.java
@@ -1,1 +1,13 @@
-..
+import java.util.Scanner;
+class dec
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+      System.out.print("Enter x(Initial) and y(Final) : ");
+      int x = sc.nextInt();
+      int y = sc.nextInt();
+      for(int i=x;i>=y;i--){   // x > y ;
+            System.out.println(" " + i);
+        }
+	}
+}
