fecda575d31d2aafb1bd6ebcebf8423e1ccc17b1
Adrima Biswas <adrimabiswas2003@gmail.com>
2025-09-17 23:13:07 +0530

    Update temp.java

--- Loop/temp.java
+++ Loop/temp.java
@@ -1,1 +1,15 @@
-../
+import java.util.*;
+class factorial{
+	public static void main(String agr[]) {
+		  Scanner sc = new Scanner(System.in);
+	      System.out.print("Enter n : ");
+	      int n = sc.nextInt();
+	      int i=1, fact=1;
+	      while(i <= n){
+	          fact *= i;
+	          i++;
+	      }
+	         System.out.print("The factorial of "+n+" is : " + fact);
+		
+	}
+}
