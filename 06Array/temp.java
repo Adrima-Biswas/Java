1df6048ac70b663cccfc05e33efa88aaeed50fc3
Adrima Biswas <adrimabiswas2003@gmail.com>
2025-09-18 23:28:44 +0530

    Update temp.java

--- 06Array/temp.java
+++ 06Array/temp.java
@@ -1,1 +1,21 @@
-a
+
+public class 2D_display
+{
+	public static void main(String[] args) {
+	    int [][] arr = {{1,2,3},
+	                {4,5,6},
+	                {7,8,9}
+	    };
+	        int n = 3;  // number of row  ==> arr.length
+	        int m = 3;  // number of column ==> arr[0].length
+	        
+	        for(int i=0 ; i<arr.length ; i++){  // i = 0 , 1
+	            for(int j=0 ; j<arr[0].length ; j++){   // j = 0
+	                System.out.print(" "+arr[i][j]);
+	            }
+	            System.out.println();
+	        }
+	    }
+	
+	}
+
