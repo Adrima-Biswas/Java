b57d6e9f844d0756dad90c70ccfe31f316a2cdc3
Adrima Biswas <adrimabiswas2003@gmail.com>
2025-09-18 23:50:12 +0530

    Update 02_2D_DiagonalSum.java

--- 06Array/02_2D_DiagonalSum.java
+++ 06Array/02_2D_DiagonalSum.java
@@ -1,1 +1,28 @@
-a
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    Scanner sc = new Scanner(System.in);
+	    int[][] arr= new int[3][3];
+	    System.out.println("enter then value: ");
+	    for(int i=0; i<arr.length; i++){
+	        for(int j=0; j<arr[0].length; j++){
+	            arr[i][j]= sc.nextInt();
+	        }
+	    }
+	    int sum=0;
+		System.out.println("Display: ");
+		for(int i=0; i<arr.length; i++){
+		    for(int j=0; j<arr[0].length; j++){
+		        if(i==j){
+		           sum = sum + arr[i][j];
+		        }
+		        System.out.print(arr[i][j]+" ");
+		    }
+		    System.out.println();
+		    
+		}
+		System.out.println("sum "+sum);
+	}
+}
