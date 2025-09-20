76837d896fcf6b04ebbb69167b85d7b82689de28
Adrima Biswas <adrimabiswas2003@gmail.com>
2025-09-20 23:26:44 +0530

    Update temp.java

--- 07Str&Char/temp.java
+++ 07Str&Char/temp.java
@@ -1,1 +1,19 @@
-a
+/*
+	1. character represent by ''. 
+	   Eg:-
+	        char var1 = 'A'; 
+	        char var2 = 'g'; 
+	        char var3 = '#'; 
+	        char var4 = '+'; 
+	        char var5 = '9'; 
+	2. Every character is associated with a specific number called ASCII code.
+*/
+
+
+class Main
+{
+	public static void main(String[] args) {
+// 	    char ch = 'K'; // 'A', '@', '1',,,,, 'a' = 97 , 'A' = 65
+		System.out.println(ch); 
+	}
+}
