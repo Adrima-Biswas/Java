fd5ede0ddd79db45fe85632b4a8672d897fcce4f
Adrima Biswas <adrimabiswas2003@gmail.com>
2025-09-25 23:41:25 +0530

    Update 03Max.java

--- 06Array/03Max.java
+++ 06Array/03Max.java
@@ -1,10 +1,7 @@
 import java.util.*;
-public class Main
-{
+public class Main {
     public static int[] maxRevenue(int[][] salesRecord) {
-        
 	    int[] answer = new int[100];
-	    
 	    for(int i=0; i<salesRecord.length; i++){
 	        int max = Integer.MIN_VALUE; // reset at evey time when you enter in a new row
 	        for(int j=0; j<salesRecord[1].length; j++) {
@@ -12,13 +9,11 @@ public class Main
 	                max = salesRecord[i][j];
 	            }
 	        }
-			
 	        answer[i] = max;
-			
 	    }
-	    
 	    return answer;
     }
+	
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
 	    int day = sc.nextInt();
@@ -29,10 +24,8 @@ public class Main
 	            records[i][j] = sc.nextInt();
 	        }
 	    }
-	    
         int[] ans = maxRevenue(records);
         System.out.println("\nmaximum revenues: "+Arrays.toString(ans));
-		
 	}
 	
 }
