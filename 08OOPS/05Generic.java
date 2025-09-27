8ea33ee1b87be3542016700510fb2ebcd5efb7e1
Adrima Biswas <adrimabiswas2003@gmail.com>
2025-09-27 18:54:25 +0530

    Update 05Generic.java

--- 08OOPS/05Generic.java
+++ 08OOPS/05Generic.java
@@ -1,1 +1,18 @@
-
+class Test <T> {
+    T num;
+}
+public class Main
+{
+	public static void main(String[] args) {
+	    Test<Integer> t1 = new Test<>(); // By adrima
+	    t1.num = 20;	    
+	    
+	    Test<Double> t2 = new Test<>();  // by mridu
+	    t2.num = 3.14;	    
+	    
+	    Test<Boolean> t3 = new Test<>();  // third-party
+	    t3.num = false;
+	    
+		System.out.println(t1.num+"\t"+t2.num+"\t"+t3.num);
+	}
+}
