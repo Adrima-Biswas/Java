bfe5fdde9e2625333b8616cdfffb3c266a6f627f
Adrima Biswas <adrimabiswas2003@gmail.com>
2025-09-27 18:56:02 +0530

    Update 05Generic.java

--- 08OOPS/05Generic.java
+++ 08OOPS/05Generic.java
@@ -5,7 +5,8 @@ public class Main
 {
 	public static void main(String[] args) {
 	    Test<Integer> t1 = new Test<>(); // By adrima
-	    t1.num = 20;	    
+	    t1.num = 20;	   
+		
 	    
 	    Test<Double> t2 = new Test<>();  // by mridu
 	    t2.num = 3.14;	    
