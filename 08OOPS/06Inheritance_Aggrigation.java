64d29e07e9b3ac53713601cca440c088dbf8d9de
Adrima Biswas <adrimabiswas2003@gmail.com>
2025-09-28 16:09:44 +0530

    Update 06Inheritance_Aggrigation.java

--- 08OOPS/06Inheritance_Aggrigation.java
+++ 08OOPS/06Inheritance_Aggrigation.java
@@ -1,1 +1,43 @@
+// Book class
+class Book {
+    String title;
+
+    Book(String title) {
+        this.title = title;
+    }
+
+    void display() {
+        System.out.println("Book: " + title);
+    }
+}
+
+// Library class contains reference to Book (Aggregation)
+class Library {
+    String name;
+    Book[] books;
+
+    Library(String name, Book[] books) {
+        this.name = name;
+        this.books = books;
+    }
+
+    void showBooks() {
+        System.out.println("Library: " + name);
+        for (Book book : books) {
+            book.display();
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Book b1 = new Book("Java Basics");
+        Book b2 = new Book("Data Structures");
+
+        Book[] bookArray = {b1, b2};
+
+        Library lib = new Library("City Library", bookArray);
+        lib.showBooks();
+    }
+}
 
